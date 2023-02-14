1b1b4dd7edc21a5836a404ae4ba8677d588c1de1
Dustin <dustinhollowaypdx@gmail.com>
2023-02-13 20:59:55 -0800

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution in this file!
+
+const employee = {
+
+    name:"Dustin",
+    streetAddress:"15 Dopeway"
+}
+;
+function updateEmployeeWithKeyAndValue(employee, key, value){;
+    return {
+        ...employee, 
+        [key]: value
+         };
+    
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){;
+    employee.name = "Sam",
+    employee.streetAddress = "12 Broadway"
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, key){;
+    const newObj = {...employee}
+    delete newObj.name
+    return newObj;      
+    }
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){;
+    delete employee.name;
+    return employee;
+}
+// Write your solution in this file
